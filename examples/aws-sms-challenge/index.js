53549dce9dc0030f2cccf7236a3259a042fd286d
laufire technologies <laufiretechnologies@gmail.com>
2019-08-07 01:28:42 +0530

    Refactored for readability.

--- examples/aws-sms-challenge/index.js
+++ examples/aws-sms-challenge/index.js
@@ -1,35 +1,34 @@
-const expiringChallenge = require('../../src');
-const awsSMSHelper = require('aws-sms-helper');
+const SMSHelper = require('aws-sms-helper');
+const ExpiringChallenge = require('../../src');
 
 /* Data */
 const ENV = Object.assign({}, process.env);
 const { accessKeyId, secretAccessKey, region, phone } = ENV;
 
 /* Delegates */
-const smsHelper = new awsSMSHelper({
+const smsHelper = new SMSHelper({
 	AWSConfig: {
 		accessKeyId, secretAccessKey, region,
 	},
 });
 
-const action = async ({ id, secret }) => {
-	await smsHelper.send({
+const action = ({ id, secret }) =>
+	smsHelper.send({
 		message: `Your OTP for <Some Company> is ${secret}`,
 		phoneNumber: id,
 		senderID: 'AlphaNum11' // #Note: In some AWS Regions, this has to be registered with AWS.
 	});
-}
 
 (async () => {
 
-	const ec = new expiringChallenge({
+	const ec = new ExpiringChallenge({
 		action,
 		secretLength: 8,
 		expiry: 60000,
 		challengeLimit: 1000,
 	});
 
-	let challenge = await ec.create({ id: phone });
+	const challenge = await ec.create({ id: phone });
 	console.log(`Challenge sent to: ${phone}`);
 	console.log(await ec.patch({ id: challenge.id, secret: challenge.secret }));
 
